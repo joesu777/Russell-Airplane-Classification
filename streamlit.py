363ba890468bb30bdaad7b4ef56305634c7464e8
joesu777 <jswan4123@gmail.com>
2026-01-03 10:31:44 +0700

    initial commit

---
+++ streamlit.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from fastai.vision.all import *
+
+st.title("Airplane Identifier")
+st.text("Built by, Russell Susanto.")
+
+
+def extract_plane_name(file_name):
+    p = str(file_name)
+    # print(p)
+    plane_name_parts = p.split('/')
+    print(plane_name_parts)
+    final_plane_name = plane_name_parts[-2]
+    return final_plane_name
+
+
+
+breed_model = load_learner("airplane_model.pkl")
+
+
+def predict(image):
+    img = PILImage.create(image)
+    pred_class, pred_idx, outputs = breed_model.predict(img)
+    return pred_class
+
+uploaded_file = st.file_uploader("Upload an image...", type=["jpg", "png", "jpeg"])
+
+if uploaded_file is not None:
+    st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+
+    prediction = predict(uploaded_file)
+    st.subheader(f"Predicted Breed: {prediction}")
+
+st.text("Built with Streamlit and FastAI, Made by russell.")
